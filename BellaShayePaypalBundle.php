47ffd6ee6807d899e83d6c1a44feb8b57839fe65
jeremy7786 <jeremy@bellashaye.com>
2013-06-30 23:03:12 -0700

    First Commit

---
+++ BellaShayePaypalBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BellaShaye\PaypalBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BellaShayePaypalBundle extends Bundle
+{
+}
---
+++ Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BellaShaye\PaypalBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('BellaShayePaypalBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
